45673cbf40339580144357d299d0da9b81f80923
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-27 17:41:02 +0100

    loadPlaces function added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,28 @@ import axios from 'axios'
 
 class App extends Component {
 
+  loadPlaces = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+    const parameters = {
+      client_id: "GVJY0ASSIKQZXNLSH3SUTUP0QHJPIX4HXNW4SH2NNAHTEJP2",
+      client_secret: "5A3ANB4RL11MWKHBA2W0KYGKDIRHERLL1XRZE3JEH1BUS4V5",
+      v: "20180323",
+      query: "coffee",
+      near: "Leicester, UK",
+      limit: 5
+    }
 
+    // Get data for coffee places from Foursquare
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          places: response.data.response.groups[0].items
+        }, this.loadMap())
+      })
+      .catch(error => {
+        alert("An error occurred fetching data from Foursquare: " + error);
+      })
+  }
 
   render() {
     return (
