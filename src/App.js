0f1448dd9807555793d3d7a506525f3f999655b9
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-27 17:39:34 +0100

    import axios to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react';
-import Filter from './components/Filter';
-import List from './components/List';
-import Map from './components/Map';
+import React, { Component } from 'react'
+import Filter from './components/Filter'
+import List from './components/List'
+import Map from './components/Map'
+import axios from 'axios'
 
 
 class App extends Component {
+
+
+
   render() {
     return (
       <div className="App">
