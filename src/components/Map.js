61cecb941c13de56b321157067e0f578f06b8443
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-27 17:13:37 +0100

    Removed loadPlaces and added initMap function

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 
-loadMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCdU03TvjRBVim4B5U3qa95CuwbVJN4Q2E&callback=initMap");
-    window.initMap = this.initMap;
-    // Shows alert when problem with auth, from: https://developers.google.com/maps/documentation/javascript/events#auth-errors
-    window.gm_authFailure = function() {
-      alert('Cannot load Google Maps! Please ensure that you have a valid Google Maps API key! Please go to https://developers.google.com/maps/documentation/javascript/get-api-key')
-      }
-  }
+class Map extends Component {
 
-  loadPlaces = () => {
-      const endPoint = "https://api.foursquare.com/v2/venues/explore?";
-      const parameters = {
-        client_id: "GVJY0ASSIKQZXNLSH3SUTUP0QHJPIX4HXNW4SH2NNAHTEJP2",
-        client_secret: "5A3ANB4RL11MWKHBA2W0KYGKDIRHERLL1XRZE3JEH1BUS4V5",
-        v: "20180323",
-        query: "coffee",
-        near: "Leicester, UK",
-        limit: 5
-      }
-
-      // Get data for coffee places from Foursquare
-      axios.get(endPoint + new URLSearchParams(parameters))
-        .then(response => {
-          this.setState({
-            places: response.data.response.groups[0].items
-          }, this.loadMap())
-        })
-        .catch(error => {
-          alert("An error occurred fetching data from Foursquare: " + error);
-        })
+  loadMap = () => {
+      loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyCdU03TvjRBVim4B5U3qa95CuwbVJN4Q2E&callback=initMap");
+      window.initMap = this.initMap;
+      // Shows alert when problem with auth, from: https://developers.google.com/maps/documentation/javascript/events#auth-errors
+      window.gm_authFailure = function() {
+        alert('Cannot load Google Maps! Please ensure that you have a valid Google Maps API key! Please go to https://developers.google.com/maps/documentation/javascript/get-api-key')
+        }
     }
 
-class Map extends Component {
+    initMap = () => {
+      let map = new window.google.maps.Map(document.getElementById('map'), {
+        center: {lat: 52.637106, lng: -1.139771},
+        zoom: 15
+      });
+    }
 
   render() {
     return(
