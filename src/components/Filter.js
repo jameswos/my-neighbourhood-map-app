3cd96e8cd5643986e64f723f9273e7afdf2d7748
James Woszczynski <jameswoszczynski@gmail.com>
2018-08-28 19:30:51 +0100

    Adding a filter using refs

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 
 class Filter extends Component {
 
+  filterUpdate() {
+    const val = this.refs.myValue.value
+  }
+
   render() {
     return(
       <header>
         <form>
           <input
             type='text'
+            ref="myValue"
             placeholder='Type to filter...'
+            onChange={this.filterUpdate.bind(this)}
           />
         </form>
       </header>
