1e6f9fd925cc8ce8bd819faedcc8476e43a84cd2
James Woszczynski <jameswoszczynski@gmail.com>
2018-10-07 16:24:05 +0100

    Shorthanded mapMarkers array in List component

--- src/components/List.js
+++ src/components/List.js
@@ -4,14 +4,14 @@ class List extends Component {
 
   render() {
 
-    const { places } = this.props;
+    const { places, mapMarkers } = this.props;
 
     const placeList = places
       .map(place => {
         return (
           // Could I set up some sort of filter / 'if else' condition here
           // to change the visibility of the markers to true if they have
-          // the same id as the key? then set the visibility of the others to false to hide them? 
+          // the same id as the key? then set the visibility of the others to false to hide them?
           <li
             key={place.venue.id}
           >
